34cba70622b171a0475da31750631818f47eed27
Iam Slaughter <jlgslaughter@gmail.com>
2018-02-12 20:30:44 -0500

    wip

---
+++ cake.js
@@ -0,0 +1,12 @@
+var maxProfit = (array) => {
+    var min = array[0];
+    var maxProfit = array[1] - array[0];
+
+    for (var i = 1 ; i < array.length; i++){
+        var currentPrice = array[i];
+        var potentialProfit = currentPrice - min;
+        maxProfit = Math.max(potentialProfit, maxProfit)
+        min = Math.min(array[i], min);
+    }
+    return maxProfit;
+}
